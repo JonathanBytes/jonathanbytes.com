d43320628f50538be1e73687467e9b3630bd1ec9
Jonathan ArchDell <jonathancardonaramirez@gmail.com>
2024-01-30 14:58:09 -0500

    Solved font problem on BlogCard

--- app/fonts.js
+++ app/fonts.js
@@ -1,5 +1,9 @@
 import { IBM_Plex_Mono, Yeseva_One, Montserrat } from 'next/font/google'
-export const ibm = IBM_Plex_Mono({ subsets: ['latin'], weight: ['400', '700'] })
+
+export const ibm = IBM_Plex_Mono({
+  subsets: ['latin'],
+  weight: ['400', '700']
+})
 export const yeseva = Yeseva_One({
   weight: '400',
   subsets: ['latin']
