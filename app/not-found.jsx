ed77c58384546a9b41e6ac93f1ff1e1e9beb5015
Jonathan ArchDell <jonathancardonaramirez@gmail.com>
2024-01-30 20:47:25 -0500

    Optimize images format

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,12 +1,12 @@
 import Image from "next/image"
-import banner404 from '../public/assets/404.png'
+import banner404 from '../public/assets/404.webp'
 
 export const metadata = {
   title: 'Not found (404)',
   description: 'Not found page, error 404'
 }
 
-export default function RootLayout({ children }) {
+export default function RootLayout() {
   return (
     <main>
       <Image width='80%' src={banner404} alt="404 JonathanBytes image" />
