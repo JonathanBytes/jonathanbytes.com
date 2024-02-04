e4bf567174ace10f295da83a6b39e5bedb9067eb
Jonathan ArchDell <jonathancardonaramirez@gmail.com>
2024-02-03 22:41:41 -0500

    Fix svg problems

--- app/components/SwitchThemeDesktop.jsx
+++ app/components/SwitchThemeDesktop.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import SwitchTheme from "./SwitchTheme";
-import { MagicWand } from "./Icons";
+import { Pallette } from "./Icons";
 
 const SwitchThemeDesktop = ({ className }) => {
   const [showSwitchMenu, setShowSwitchMenu] = useState('hidden')
@@ -14,7 +14,7 @@ const SwitchThemeDesktop = ({ className }) => {
   return (
     <div className={className}>
       <button className='rounded-lg w-8 h-8 p-1 text-accent' onClick={handleClick}>
-        <MagicWand />
+        <Pallette />
       </button>
       <SwitchTheme className={`${showSwitchMenu} absolute top-10 -translate-x-[3.75rem] bg-text p-2 rounded-lg z-10`} />
     </div>
