f6316fb7b0e3d340d965705391963e0ce80e9ad4
Jonathan ArchDell <jonathancardonaramirez@gmail.com>
2024-09-01 21:42:46 -0500

    Fix tailwind config, not tracking all components

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,8 @@
 module.exports = {
   content: [
     './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './app/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/components/**/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   darkMode: 'selector',
   theme: {
