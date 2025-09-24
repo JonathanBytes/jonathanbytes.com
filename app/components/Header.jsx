6811ebf19eaab60852695716784dbd5e359c0240
Jonathan Windows machine <jonathancardonaramirez@gmail.com>
2025-09-23 21:09:47 -0500

    fix: adjust MobileNavBar height handling for improved visibility and consistency

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -42,7 +42,7 @@ export default function Header({ initialUserColors }) {
       )}
 
       <MobileNavBar
-        className="mobile-nav md:hidden text-xl font-bold fixed top-0 right-0 h-screen min-h-dvh min-w-40 bg-background/95 backdrop-blur-xs z-20 pt-20"
+        className="mobile-nav md:hidden text-xl font-bold fixed top-0 right-0 min-w-40 bg-background/95 backdrop-blur-xs z-20 pt-20"
         data-visible={isMobileNavVisible}
         initialUserColors={initialUserColors}
         onClick={handleToggle}
